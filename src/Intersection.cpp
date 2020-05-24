f60418e937b0d4a0e3d47ae0f17ad602c10a91c7
Matt Buckley <matt.buckley1755@gmail.com>
2020-05-24 21:34:24 +0000

    Finished lesson 3 exercise

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <future>
 #include <random>
+#include <mutex>
 
 #include "Street.h"
 #include "Intersection.h"
@@ -10,22 +11,22 @@
 
 /* Implementation of class "WaitingVehicles" */
 
-// L3.1 : Safeguard all accesses to the private members _vehicles and _promises with an appropriate locking mechanism, 
-// that will not cause a deadlock situation where access to the resources is accidentally blocked.
-
 int WaitingVehicles::getSize()
 {
-    return _vehicles.size();
+    std::lock_guard<std::mutex> lck(_mutex);
+  	return _vehicles.size();
 }
 
 void WaitingVehicles::pushBack(std::shared_ptr<Vehicle> vehicle, std::promise<void> &&promise)
 {
+    std::lock_guard<std::mutex> lck(_mutex);
     _vehicles.push_back(vehicle);
     _promises.push_back(std::move(promise));
 }
 
 void WaitingVehicles::permitEntryToFirstInQueue()
 {
+    std::lock_guard<std::mutex> lck(_mutex);
     // get entries from the front of both queues
     auto firstPromise = _promises.begin();
     auto firstVehicle = _vehicles.begin();
@@ -69,10 +70,10 @@ std::vector<std::shared_ptr<Street>> Intersection::queryStreets(std::shared_ptr<
 // adds a new vehicle to the queue and returns once the vehicle is allowed to enter
 void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 {
-    // L3.3 : Ensure that the text output locks the console as a shared resource. Use the mutex _mtxCout you have added to the base class TrafficObject in the previous task. Make sure that in between the two calls to std-cout at the beginning and at the end of addVehicleToQueue the lock is not held. 
-
+    std::unique_lock<std::mutex> lck(_mtxCout);
     std::cout << "Intersection #" << _id << "::addVehicleToQueue: thread id = " << std::this_thread::get_id() << std::endl;
-
+    lck.unlock();
+  
     // add new vehicle to the end of the waiting line
     std::promise<void> prmsVehicleAllowedToEnter;
     std::future<void> ftrVehicleAllowedToEnter = prmsVehicleAllowedToEnter.get_future();
@@ -80,6 +81,7 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 
     // wait until the vehicle is allowed to enter
     ftrVehicleAllowedToEnter.wait();
+    lck.lock();
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
 }
 
--- src/TrafficObject.cpp
+++ src/TrafficObject.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <chrono>
+#include <mutex>
 #include <algorithm>
 #include "TrafficObject.h"
 
 // init static variable
 int TrafficObject::_idCnt = 0;
-
-// L3.2 : Add a static mutex to the base class TrafficObject (called _mtxCout) and properly instantiate it in the source file. This mutex will be used in the next task to protect standard-out. 
+std::mutex TrafficObject::_mtxCout;
 
 void TrafficObject::setPosition(double x, double y)
 {
