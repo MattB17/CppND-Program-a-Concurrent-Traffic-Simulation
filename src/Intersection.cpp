329a7e797c5cdd0ae213c18531f9b1f8c5b20b05
MattB17 <matthewbuckley@Matthews-MacBook-Air.local>
2020-05-30 19:26:02 -0400

    Fixing while loop to ensure it only stops when the current phase is green

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -86,7 +86,7 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 
     lck.unlock();
 
-    while (_trafficLight.getCurrentPhase() == TrafficLightPhase::red) {
+    while (_trafficLight.getCurrentPhase() != TrafficLightPhase::green) {
       _trafficLight.waitForGreen();
     }
 }
